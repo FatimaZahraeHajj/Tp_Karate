4a0a3abe4213c5830670ce906603cacc16f75188
Fatima Zahrae HAJJOUBI <hajjoubifatimazahrae@gmail.com>
2025-04-04 11:38:38 +0200

    mise à jour 1

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -2,9 +2,9 @@ package runner;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RunnerTest {
 
@@ -12,6 +12,6 @@ public class RunnerTest {
     public void testParallel() {
         Results results = Runner.path("classpath:features")
                 .parallel(1);
-        assertTrue("Il y a des échecs : " + results.getErrorMessages(), results.getFailCount() == 0);
+        assertTrue(results.getFailCount() == 0, "Il y a des échecs : " + results.getErrorMessages());
     }
 }
